69a580832e3885bcc06e2ca25db351dba13c8abb
NoupSange <69147336+NoupSange@users.noreply.github.com>
2021-01-07 00:32:26 +0300

    Add files via upload

---
+++ JavaScrit-Clock/clock.js
@@ -0,0 +1,19 @@
+const deg = 6;
+const hr = document.getElementById('hr');
+const mn = document.getElementById('mn');
+const sc = document.getElementById('sc');
+
+
+
+
+function clock() {
+    let  day = new Date();
+    let  hh = day.getHours()*30;
+    let  mm = day.getMinutes() *deg;
+    let  ss = day.getSeconds() *deg;
+
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+} 
+setInterval(clock,1000)
